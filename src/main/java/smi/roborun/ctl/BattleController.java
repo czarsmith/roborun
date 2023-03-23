c0f7fdcb99225d09be6e7f75ccfca64ec029d4f1
Czar Smith <czarsmith@gmail.com>
2023-03-23 16:34:34 -0500

    cleanup

--- src/main/java/smi/roborun/ctl/BattleController.java
+++ src/main/java/smi/roborun/ctl/BattleController.java
@@ -5,13 +5,17 @@ import java.awt.Container;
 import java.awt.Window;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JSlider;
@@ -33,6 +37,7 @@ public class BattleController extends BattleAdaptor implements Runnable {
   private JSlider tpsSlider;
   private List<Integer> tpsTicks;
   private Window robocodeWin;
+  File robocodeDir;
 
   public BattleController() {
     // The TPS slider isn't linear so we have to configure each of the individual tick marks
@@ -42,6 +47,16 @@ public class BattleController extends BattleAdaptor implements Runnable {
     tpsTicks.addAll(List.of(30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50));
     tpsTicks.addAll(List.of(53, 56, 59, 62, 65, 70, 75, 80, 85, 90, 95, 100));
     tpsTicks.addAll(List.of(110, 130, 150, 200, 300, 500, 750, 1000, Integer.MAX_VALUE));
+
+    robocodeDir = new File("/home/czarsmith/robocode");
+    
+    RobocodeEngine.setLogMessagesEnabled(false);
+    engine = new RobocodeEngine(robocodeDir);
+    engine.addBattleListener(this);
+  }
+
+  public List<RobotSpecification> getRobots() {
+    return Arrays.asList(engine.getLocalRepository());
   }
 
   public void execute() {
@@ -52,7 +67,10 @@ public class BattleController extends BattleAdaptor implements Runnable {
     robocodeWin = until(() -> Stream.of(Window.getWindows())
       .filter(w -> "net.sf.robocode.ui.dialog.RobocodeFrame".equals(w.getClass().getName()))
       .findAny().orElse(null));
-
+    ((JFrame)robocodeWin).setUndecorated(true);
+    
+    until(() -> findComponents(robocodeWin, c -> c instanceof JMenu)).forEach(c -> ((JMenu)c).setVisible(false));
+    
     // Find the TPS Slider
     tpsSlider = until(() -> (JSlider)findComponent(robocodeWin, c -> c instanceof JSlider));
 
@@ -70,8 +88,6 @@ public class BattleController extends BattleAdaptor implements Runnable {
   }
 
   public void run() {
-    File robocodeDir = new File("/home/czarsmith/robocode");
-    
     int tps = 25;
     int numberOfRounds = 3;
     int battlefieldWidth = 800;
@@ -84,14 +100,11 @@ public class BattleController extends BattleAdaptor implements Runnable {
     config.setWindowSize(0, 0, 170 + battlefieldWidth, 140 + battlefieldHeight);
     config.apply();
 
-    RobocodeEngine.setLogMessagesEnabled(false);
-    engine = new RobocodeEngine(robocodeDir);
-    engine.addBattleListener(this);
-    engine.setVisible(true);
-
     BattlefieldSpecification battlefield = new BattlefieldSpecification(battlefieldWidth, battlefieldHeight);
     RobotSpecification[] selectedRobots = engine.getLocalRepository("sample.RamFire,sample.Corners");
     BattleSpecification battleSpec = new BattleSpecification(numberOfRounds, battlefield, selectedRobots);
+
+    engine.setVisible(true);
     engine.runBattle(battleSpec, true);
     engine.close();
     running = false;
@@ -99,27 +112,33 @@ public class BattleController extends BattleAdaptor implements Runnable {
 
   private <T> T until(Supplier<T> supplier) {
     T result = supplier.get();
-    while (result == null && running) {
-      try { Thread.sleep(200); } catch (Exception e) {}
-      result = supplier.get();
+    while ((result == null || (result instanceof List && ((List<?>)result).isEmpty())) && running) {
+      try {
+        Thread.sleep(200);
+        result = supplier.get();
+      } catch (Exception e) {}
     }
     return result;
   }
 
   private Component findComponent(Component comp, Predicate<Component> matcher) {
+    return findComponents(comp, matcher).get(0);
+  }
+
+  private List<Component> findComponents(Component comp, Predicate<Component> matcher) {
     if (matcher.test(comp)) {
-      return comp;
-    } else if (comp instanceof JMenu menu) {
-      return IntStream.range(0, menu.getItemCount()).mapToObj(i -> menu.getItem(i)).filter(Objects::nonNull)
-        .map(c -> findComponent(c, matcher)).filter(Objects::nonNull).findAny().orElse(null);
-    } else if (comp instanceof JMenuItem menuitem) {
-      return Stream.of(menuitem.getSubElements()).map(o -> o.getComponent()).filter(Objects::nonNull)
-        .map(c -> findComponent(c, matcher)).filter(Objects::nonNull).findAny().orElse(null);
+      return Arrays.asList(comp);
+    } else if (comp instanceof JMenu) {
+      return IntStream.range(0, ((JMenu)comp).getItemCount()).mapToObj(i -> ((JMenu)comp).getItem(i)).filter(Objects::nonNull)
+        .flatMap(c -> findComponents(c, matcher).stream()).filter(Objects::nonNull).collect(Collectors.toList());
+    } else if (comp instanceof JMenuItem) {
+      return Stream.of(((JMenuItem)comp).getSubElements()).map(o -> o.getComponent()).filter(Objects::nonNull)
+        .flatMap(c -> findComponents(c, matcher).stream()).filter(Objects::nonNull).collect(Collectors.toList());
     } else if (comp instanceof java.awt.Container) {
       return Stream.of(((Container)comp).getComponents())
-        .map(c -> findComponent(c, matcher)).filter(Objects::nonNull).findAny().orElse(null);
+        .flatMap(c -> findComponents(c, matcher).stream()).filter(Objects::nonNull).collect(Collectors.toList());
     } else {
-      return null;
+      return Collections.emptyList();
     }
   }
 
